06822c992f1b087a87fd40061eed24839f0a5232
Jad Haddad <jad.haddad@zto-technology.com>
2019-05-13 15:18:05 +0200

    first commit

---
+++ facedetect.py
@@ -0,0 +1,34 @@
+import cv2
+import sys
+import argparse
+import os
+
+dirname = os.path.dirname(__file__)
+
+CASCADES = [
+    'haarcascade_frontalface_alt2.xml',
+    'haarcascade_frontalface_alt_tree.xml',
+    'haarcascade_frontalface_alt.xml',
+    'haarcascade_frontalface_default.xml',
+    'haarcascade_profileface.xml'
+]
+
+def detectFace(filepath):
+    """ detect faces in image
+    """
+    im = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    if im is None:
+        print("cannot load input image {}".format(filepath))
+    im = cv2.equalizeHist(im)
+    for cascade in CASCADES:
+        fc = cv2.CascadeClassifier(os.path.join(dirname,'haarcascades',cascade))
+        faces = fc.detectMultiScale(im)
+        if len(faces) > 1:
+            print(faces)
+            sys.exit(0)
+    sys.exit(1)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file')
+args = parser.parse_args()
+detectFace(args.file)
